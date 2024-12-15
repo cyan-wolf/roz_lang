99518d3afb2132cacd08c656dccf99c716d89d4d
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2024-12-15 10:54:45 -0400

    almost finished `Resolver`

--- src/roz/resolver.rs
+++ src/roz/resolver.rs
@@ -23,28 +23,67 @@ impl Resolver {
 
     fn resolve_stmt(&mut self, stmt: Stmt) {
         match stmt {
-            Stmt::Expr(expr) => todo!(),
-            Stmt::Print(expr) => todo!(),
+            Stmt::Expr(expr) => {
+                self.resolve_expr(expr);
+            },
+            Stmt::Print(expr) => {
+                self.resolve_expr(expr);
+            },
             Stmt::DeclareVar(name, init) => {
                 self.declare(name.clone());
                 self.resolve_expr(init);
                 self.define(name);
             },
-            Stmt::Block(vec) => todo!(),
-            Stmt::If(expr, vec, vec1) => todo!(),
-            Stmt::While(expr, vec) => todo!(),
-            Stmt::For(stmt, expr, expr1, vec) => todo!(),
-            Stmt::Fun(_, vec, vec1) => todo!(),
-            Stmt::Return(token, expr) => todo!(),
+            Stmt::Block(statements) => {
+                self.resolve_block(statements);
+            },
+            Stmt::If(cond, then_branch, else_branch) => {
+                self.resolve_expr(cond);
+                self.resolve_block(then_branch);
+                
+                if let Some(else_branch) = else_branch {
+                    self.resolve_block(else_branch);
+                }
+            },
+            Stmt::While(cond, body) => {
+                self.resolve_expr(cond);
+                self.resolve_block(body);
+            },
+            Stmt::For(init, cond, side_effect, for_block) => {
+                self.resolve_stmt(*init);
+                self.resolve_expr(cond);
+                self.resolve_expr(side_effect);
+
+                self.resolve_block(for_block);
+            },
+            Stmt::Fun(name, params, body) => {
+                self.declare(name.clone());
+                self.define(name);
+
+                self.resolve_fun(params, body);
+            },
+            Stmt::Return(_, expr) => {
+                self.resolve_expr(expr);
+            },
         }
     }
 
     fn resolve_expr(&mut self, expr: Expr) {
         match expr {
-            Expr::Literal(value) => todo!(),
-            Expr::Unary(token, expr) => todo!(),
-            Expr::Binary(expr, token, expr1) => todo!(),
-            Expr::Grouping(expr) => todo!(),
+            Expr::Literal(_value) => {
+                // Do nothing, since a literal value
+                // does not contain subexpressions.
+            },
+            Expr::Unary(_, expr) => {
+                self.resolve_expr(*expr);
+            },
+            Expr::Binary(left, _, right) => {
+                self.resolve_expr(*left);
+                self.resolve_expr(*right);
+            },
+            Expr::Grouping(expr) => {
+                self.resolve_expr(*expr);
+            },
             Expr::Var(ident_token) => {
                 if let Some(scope) = self.scopes.last() {
                     let ident = ident_token.extract_ident();
@@ -58,20 +97,60 @@ impl Resolver {
 
                 self.resolve_local(ident_token);
             },
-            Expr::Assign(token, expr) => todo!(),
-            Expr::Call(expr, vec, token) => todo!(),
+            Expr::Assign(lvalue, rvalue) => {
+                self.resolve_expr(*rvalue);
+                self.resolve_local(lvalue);
+            },
+            Expr::Call(callee, args, _) => {
+                self.resolve_expr(*callee);
+                for arg in args {
+                    self.resolve_expr(arg);
+                }
+            },
         }
     }
 
+    /// Same as `Resolver::resolve_stmt`, but resolves the statements 
+    /// in a new scope.
+    fn resolve_block(&mut self, statements: Vec<Stmt>) {
+        self.begin_scope();
+        self.resolve(statements);
+        self.end_scope();
+    }
+
+    /// Look for the variable references by `ident` starting from the 
+    /// outermost scope and checking all the way until the most general scope.
+    /// Stops looking just before it reaches the global scope.
     fn resolve_local(&mut self, ident: Token) {
         for i in (0..self.scopes.len()).rev() {
             if self.scopes[i].contains_key(ident.extract_ident()) {
+                let outermost_scope_idx = self.scopes.len() - 1;
+                
+                // The number of scopes between the current (outermost) scope
+                // and the scope where the variable was found (in this case, `i`).
+                let scope_jumps = outermost_scope_idx - i;
                 //self.interpreter.resolve(Expr::Var(ident), ...)
-                todo!()
+                todo!();
+
+                // Stop searching since the variable was found.
+                return;
             }
         }
+    }
 
-        todo!()
+    fn resolve_fun(&mut self, params: Vec<Token>, body: Vec<Stmt>) {
+        self.begin_scope();
+
+        for param in params {
+            let ident = param.extract_ident().to_owned();
+            self.declare(ident.clone());
+            self.define(ident);
+        }
+        
+        // No need to use `Resolver::resolve_block`, since we already
+        // began a scope in this function (`Resolver::resolve_fun`).
+        self.resolve(body);
+        self.end_scope();
     }
 
     fn begin_scope(&mut self) {
